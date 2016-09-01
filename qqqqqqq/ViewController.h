6e3bdb6767cbaccc79ec0adf2f1f82e55e1c76fa
dgrytuyukjhlkjlgsfs <mac@huangxiangmin.local>
2016-09-01 10:16:55 +0800

    朱江磊
    第一次使用

---
+++ qqqqqqq/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  qqqqqqq
+//
+//  Created by MAC on 16/9/1.
+//  Copyright © 2016年 ZJL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ qqqqqqq/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  qqqqqqq
+//
+//  Created by MAC on 16/9/1.
+//  Copyright © 2016年 ZJL. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
